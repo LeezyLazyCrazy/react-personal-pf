500248218ff3187e3c9ce0dbd790a8ab08da8967
LeezyLazyCrazy <leeg1dance@gmail.com>
2022-04-07 18:04:51 +0900

    test Apr.7 2022

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {RiUserStarLine} from 'react-icons/ri'
-import {RiTodoLine} from 'react-icons/ri'
 import {RiServiceLine} from 'react-icons/ri'
 import {MdOutlineMonitor} from 'react-icons/md'
 import {FiPhoneCall} from 'react-icons/fi'
@@ -14,7 +13,6 @@ const Nav = () => {
     <nav>
       <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav ==='#home' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''}><RiUserStarLine/></a>
-      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav ==='#experience' ? 'active' : ''}><RiTodoLine/></a>
       <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav ==='#services' ? 'active' : ''}><RiServiceLine/></a>
       <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav ==='#portfolio' ? 'active' : ''}><MdOutlineMonitor/></a>
       <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav ==='#contact' ? 'active' : ''}><FiPhoneCall/></a>
